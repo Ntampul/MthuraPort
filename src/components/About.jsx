e89a99f335fbef5655f0f384045dde683be24c0c
Ntampul <ntampulamthunzi@gmail.com>
2023-08-24 15:10:01 +0200

    my cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,1 +1,38 @@
+// src/components/About.js
+import React from 'react';
+import { FaCode, FaDatabase, FaServer } from 'react-icons/fa'; // Import icons
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h2>About Me</h2>
+      <p>
+        Hi there! I'm [Your Name], a passionate developer with a knack for building awesome web applications.
+      </p>
+      <h2>Skills</h2>
+      <div className="skills-container">
+        <div className="skill">
+          <FaCode className="skill-icon" />
+          <h3>Frontend Development</h3>
+          <p>I specialize in creating engaging and responsive user interfaces using modern frontend technologies.</p>
+        </div>
+        <div className="skill">
+          <FaDatabase className="skill-icon" />
+          <h3>Database Management</h3>
+          <p>I'm experienced in designing and managing databases, ensuring efficient data storage and retrieval.</p>
+        </div>
+        <div className="skill">
+          <FaServer className="skill-icon" />
+          <h3>Backend Development</h3>
+          <p>I have a strong backend development background, building robust server-side logic for applications.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
+
+
+
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@
 //import Typed from 'typed.js';
 //import Video from './Video.mp4';
 import { FaGithub, FaLinkedin, FaFacebook, FaInstagram } from 'react-icons/fa6';
-
+import './Home.css';
 
 const Home = () => {
 
@@ -17,12 +17,7 @@ const Home = () => {
             <h2 className="home__title">
                <span className="multiple-text"></span>
             </h2>
-            <div className="home__description">
-              <p>
-              Hi. i’M MTHUNZI NTAMPULA
-Welcome to my portfolio as i am looking for a job as a software DEVELOPER..
-              </p>
-            </div>
+            
             <div className="home__social social-media">
               <a href="https://github.com/Ntampul" className="social-media__link animated">
                 <FaGithub className="icon" /> <br></br>
@@ -33,16 +28,24 @@ Welcome to my portfolio as i am looking for a job as a software DEVELOPER..
               <a href="https://web.facebook.com/profile.php?id=100063655705791" className="social-media__link animated">
                 <FaFacebook className="icon" /><br></br>
               </a>
-              <a href="https://www.instagram.com/scott_vincent98/" className="social-media__link animated">
+              <a href="https://www.instagram.com/bhut_mthunz/" className="social-media__link animated">
                 <FaInstagram className="icon" /><br></br>
               </a>
             </div>
-            <a href="mbongwe cv (3).pdf" className="btn">Download CV</a>
+         
           </div>
         </div>
+        <div className="home__description">
+                                   <p>
+                                    Hi. i’M MTHUNZI NTAMPULA
+                                     Welcome to my portfolio as i am looking for a job as a software DEVELOPER..
+                                   </p>
+            </div>
         <div className="home__image">
-          <img src="mthura.png" alt="home-image" />
+          <img src="https://i.ibb.co/3SVW3Ch/Whats-App-Image-2023-08-21-at-14-23-34.jpg"alt="home-image" />
+         <div className="cv"> <a href="mthunzi.pdf" className="btn">Download CV</a></div>
         </div>
+                                  
       </section>
   );
 };
