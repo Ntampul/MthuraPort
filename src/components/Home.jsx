1bc75a4feac4db1d9727c30aba4e5415bb9df9e6
Ntampul <ntampulamthunzi@gmail.com>
2023-09-18 13:28:38 +0200

    responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,12 +20,7 @@ const Home = () => {
 
         <div className="home__content rain">
           <div className="home__content">
-            {/* <h2 className="home__title"></h2>
-            <h1 className="home__name"></h1> */}
-            {/* <h2 className="home__title">
-               <span className="multiple-text"></span>
-            </h2> */}
-
+      
             <div className="home__social social-media">
               <a href="https://github.com/Ntampul" className="social-media__link animated">
                 <FaGithub className="icon" /> <br></br>
@@ -36,9 +31,7 @@ const Home = () => {
               <a href="https://dribbble.com/account/profile" className="social-media__link animated">
                 <AiFillDribbbleCircle className="icon" /><br></br>
               </a>
-              {/* <a href="https://www.instagram.com/bhut_mthunz/" className="social-media__link animated">
-                <FaInstagram className="icon" /><br></br>
-              </a> */}
+            
             </div>
           </div>
         </div>
