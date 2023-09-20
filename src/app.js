25ce14db72c4e5c9a5582a59ccf4b9e215a111d2
Ntampul <ntampulamthunzi@gmail.com>
2023-09-20 13:07:19 +0200

    responsive

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contacts from './components/Contacts';
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Header from './components/Header';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Projects from './components/Projects';
+// import Contacts from './components/Contacts';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contacts" component={Contacts} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Header />
+//         <Switch>
+//           <Route path="/" exact component={Home} />
+//           <Route path="/about" component={About} />
+//           <Route path="/projects" component={Projects} />
+//           <Route path="/contacts" component={Contacts} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
